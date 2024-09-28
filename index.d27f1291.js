71893bfe75c584ae3a32e524f884391012ca8cca
Mykhailo Lapchynskyi <mykhailolapchynskyi@Dikaas-MacBook-Pro.local>
2024-09-28 17:12:35 +0200

    make build

---
+++ index.d27f1291.js
@@ -0,0 +1,2 @@
+var e=function(e,o,r){for(var t=e.rows,c=0;c<t.length;c++){var l=t[c].cells[o].cloneNode(!0);t[c].insertBefore(l,t[c].cells[r])}},o=document.querySelector("thead"),r=document.querySelector("tbody"),t=document.querySelector("tfoot");e(o,1,4),e(r,1,4),e(t,1,4);
+//# sourceMappingURL=index.d27f1291.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,17 @@
+"use strict";
+const copyColumn = (table, sourceColumnIndex, targetColumnIndex)=>{
+    const rows = table.rows;
+    for(let i = 0; i < rows.length; i++){
+        const sourceCell = rows[i].cells[sourceColumnIndex];
+        const newCell = sourceCell.cloneNode(true);
+        rows[i].insertBefore(newCell, rows[i].cells[targetColumnIndex]);
+    }
+};
+const thead = document.querySelector("thead");
+const tbody = document.querySelector("tbody");
+const tfoot = document.querySelector("tfoot");
+copyColumn(thead, 1, 4);
+copyColumn(tbody, 1, 4);
+copyColumn(tfoot, 1, 4);
+
+//# sourceMappingURL=index.f75de5e1.js.map
